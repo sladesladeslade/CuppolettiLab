19cb3168025f37f6ec8a8ea3875a6b16935b4578
sladesladeslade <spbrooks4@gmail.com>
2023-04-09 01:08:05 -0400

    updates and need to fix freqs

--- farField/dataTest.py
+++ farField/dataTest.py
@@ -7,26 +7,20 @@ import nptdms as nptd
 import scipy as sp
 import matplotlib.pyplot as plt
 
+# read in tdms
 tdms_file = nptd.TdmsFile.read("C:\\Users\\spbro\\OneDrive - University of Cincinnati\\Cuppoletti Lab"
                                "\\NearFieldAcousticDuctedRotor\\slade mic data\\20220725\\ducted\\tm0.50"
                                "\\mic6inplane\\data0.tdms")
-# for group in tdms_file.groups():
-    # group_name = group.name
-    # for channel in group.channels():
-    #     channel_name = channel.name
-    #     # Access dictionary of properties:
-    #     properties = channel.properties
-    #     # Access numpy array of data for channel:
-    #     data = channel[:]
 
+# set up collection info
 fs = 204800     # sampling frequency
 samptime = 5        # sampling time
 N = fs*samptime      # number of data points
 pref = 20e-6    # ref p for calcs
 
+# make data array from file
 for group in tdms_file.groups():
-
-    # make data array
+    # make data array for each group
     data = np.empty([len(group.channels()), N])
 
     # loop thru to store data in array
@@ -39,25 +33,21 @@ for group in tdms_file.groups():
             data[i, k] = channel[k]
         i += 1
 
+# get number of mics
 nmics = len(data)
 
-# correct data w/ correction factor
+# correct data w/ correction factors
 corFac = np.array([0.9964, 0.9945, 0.99894, 0.99841, 1.00117, 0.99957, 0.99798, 0.9902])
 corData = data.copy()
 for i in range(nmics):
     corData[i, :] = data.copy()[i, :]*corFac[i]
 
 # butterworth filter on pressure vals
-# critical freqs
-Wl = 100
+Wl = 100        
 Wh = 100000
-critfreqs = np.array([Wl, Wh])
-
-# nyquist freq
-fh = fs/2
-
-# bworth order
-border = 5
+critfreqs = np.array([Wl, Wh])      # critical freqs
+fh = fs/2       # nyquist freq
+border = 5      # bworth order
 
 # setup bworth
 [b, a] = sp.signal.butter(border, critfreqs/fh, "bandpass")
@@ -74,8 +64,6 @@ T = samptime/(fs*n)
 fft = np.empty([bins, n//2])
 rms = np.empty(bins)
 spl = np.empty(bins)
-tempfreqs = np.fft.fftfreq(n, T)[:n//2]
-freqs = np.empty(bins)
 for i in range(0, N, n):
     buck = i//n
     # take the fft
