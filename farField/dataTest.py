9a2bd329b9289a1a48000e3339977af2a54c9374
sladesladeslade <spbrooks4@gmail.com>
2023-04-16 13:44:33 -0400

    DILIP MOMENT!!!

--- farField/dataTest.py
+++ farField/dataTest.py
@@ -72,7 +72,7 @@ pDataens = np.array_split(pData[6], n)
 # do fft
 fft = np.array([np.abs(np.fft.fft(pDataens[i])) for i in range(n)])
 # RMS
-rms = np.sqrt(np.sum((2*fft[i]/bins)**2)/n)
+rms = np.sqrt(np.sum((2*fft/bins)**2, axis=0)/n)
 # SPL
 spl = 20*np.log10(rms[:bins//2]/pref)
 # plotting
